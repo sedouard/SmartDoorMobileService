01ce7c09af889e0df8ed0ad4f6d17bfbdb311923
windowsazure <windowsazure>
2014-03-25 07:43:21 +0000

    Committing update from request /scmvfs/service/scheduler/doorBellRingListener.js

--- service/scheduler/doorBellRingListener.js
+++ service/scheduler/doorBellRingListener.js
@@ -1,3 +1,23 @@
 function doorBellRingListener() {
-    console.warn("You are running an empty scheduled job. Update the script for job 'doorBellRingListener' or disable the job.");
+    var azure = require('azure');
+    var nconf = require('nconf');
+    nconf.argv()
+        .env()
+        .file({ file: 'Web.config.json' });
+    
+    
+    var sb = azure.createServiceBusService(nconf.get("SmartDoor.ServiceBus.ConnectionString"));
+    //var hub = new azure.NotificationHubService(nconf.get("SmartDoor.ServiceBus.DoorBellNotificationHubName"),
+    //    nconf.get("SmartDoor.ServiceBus.DoorBellNotificationConnectionString"));
+    
+    listenForMessages();
+    
+    function listenForMessages() {
+        sb.receiveQueueMessage(nconf.get("SmartDoor.ServiceBus.IncomingNotificationQueue"), { timeoutIntervalInS: 90 }, 
+        function(err, data) {
+            //TODO: Send push notification from here
+            console.log(data);
+            listenForMessages();
+        });
+    }
 }
