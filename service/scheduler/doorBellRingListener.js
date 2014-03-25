fac5eb1b2c5ec9b1a68377bcaed13522b03fad98
windowsazure <windowsazure>
2014-03-25 07:45:55 +0000

    Committing update from request /scmvfs/service/scheduler/doorBellRingListener.js

--- service/scheduler/doorBellRingListener.js
+++ service/scheduler/doorBellRingListener.js
@@ -6,14 +6,14 @@ function doorBellRingListener() {
         .file({ file: 'Web.config.json' });
     
     
-    var sb = azure.createServiceBusService(nconf.get("SmartDoor.ServiceBus.ConnectionString"));
+    var sb = azure.createServiceBusService(nconf.get("Endpoint=sb://dpeproject.servicebus.windows.net/;SharedAccessKeyName=servicepolicy;SharedAccessKey=Xn1mYsNIRj47xd25AKeVa2Ant6eLC+Br0xrNfqQbhO4="));
     //var hub = new azure.NotificationHubService(nconf.get("SmartDoor.ServiceBus.DoorBellNotificationHubName"),
     //    nconf.get("SmartDoor.ServiceBus.DoorBellNotificationConnectionString"));
     
     listenForMessages();
     
     function listenForMessages() {
-        sb.receiveQueueMessage(nconf.get("SmartDoor.ServiceBus.IncomingNotificationQueue"), { timeoutIntervalInS: 90 }, 
+        sb.receiveQueueMessage(nconf.get("arduino"), { timeoutIntervalInS: 90 }, 
         function(err, data) {
             //TODO: Send push notification from here
             console.log(data);
