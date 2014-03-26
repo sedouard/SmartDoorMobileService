bb07ba8c43e13854946d923e7648ac08758f21de
windowsazure <windowsazure>
2014-03-26 00:30:42 +0000

    Committing update from request /scmvfs/service/scheduler/doorBellRingListener.js

--- service/scheduler/doorBellRingListener.js
+++ service/scheduler/doorBellRingListener.js
@@ -1,19 +1,3 @@
-var azure = require('azure');
 function doorBellRingListener() {
-
-    var sb = azure.createServiceBusService("Endpoint=sb://dpeproject.servicebus.windows.net/;SharedAccessKeyName=servicepolicy;SharedAccessKey=Xn1mYsNIRj47xd25AKeVa2Ant6eLC+Br0xrNfqQbhO4=");
-    //var hub = new azure.NotificationHubService(nconf.get("SmartDoor.ServiceBus.DoorBellNotificationHubName"),
-    //    nconf.get("SmartDoor.ServiceBus.DoorBellNotificationConnectionString"));
-    
-    listenForMessages();
-    
-    function listenForMessages() {
-        sb.receiveQueueMessage("arduino", { timeoutIntervalInS: 90 }, 
-        function(err, data) {
-            //TODO: Send push notification from here
-            console.log('Recieved ring notification for doorbell: ' + data.doorBellID);
-            console.log(data);
-            listenForMessages();
-        });
-    }
+    console.warn("You are running an empty scheduled job. Update the script for job 'doorBellRingListener' or disable the job.");
 }
