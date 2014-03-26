e9d032a06d4aafdf89341886c8b30ce629073399
sedouard <steven.edouard1@gmail.com>
2014-03-26 11:11:04 -0700

    adding mongoose shared script since we'll have to use mongoose models everywhere

---
+++ service/shared/mongooseschemas.js
@@ -0,0 +1,25 @@
+exports.doorbellSchema = mongoose.Schema({
+    		doorBellID: String,
+    		users: [{
+                    id : String,
+                    mobileDevices: [{
+                        deviceId : String,
+                        channel : String
+                    }]
+                }
+            ],
+    		photos: [{
+                blobPointer: String,
+                timestamp: String
+            }]
+		});
+exports.getDoorBellModel = function() {
+	//Note, you should compile your models globally, as subsequent api calls may cause
+	//errors as you can only do this once per node instance.
+	var DoorBell = mongoose.model('DoorBell');
+	
+	if(!DoorBell){
+		DoorBell = mongoose.model('DoorBell', exports.doorbellSchema);
+	}
+};
+//Note, you sho
