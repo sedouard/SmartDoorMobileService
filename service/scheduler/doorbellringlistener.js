3f419426bdb1706db7f9fb56a656c44d62301b73
sedouard <steven.edouard1@gmail.com>
2014-03-26 22:26:40 -0700

    data.body is the correct api to get the data from the message

--- service/scheduler/doorbellringlistener.js
+++ service/scheduler/doorbellringlistener.js
@@ -16,7 +16,7 @@ function doorBellRingListener() {
         sb.receiveQueueMessage("arduino", { timeoutIntervalInS: 90 }, 
         function(err, data) {
 			if(!err){
-                console.log('Recieved notification: ' + data);
+                console.log('Recieved notification: ' + data.body.doorbellID);
 			    console.log('Connecting to mongodb');
                 
 			    mongoose.connect('mongodb://MongoLab-4q:X7TH5fVZWynS6qUM1rht7olpktsJgNr94_ArcTVwHqs-@ds030607.mongolab.com:30607/MongoLab-4q');
@@ -25,21 +25,21 @@ function doorBellRingListener() {
                 db.on('error', console.error.bind(console, 'connection error:'));
                 db.once('open', function () {
                     console.log("Sucessfully Logged into mongo");
-                    console.log('Looking for doorBellID ' + data.doorbellID + ' in mongo');
+                    console.log('Looking for doorBellID ' + data.body.doorbellID + ' in mongo');
         
                     //Query for the speicfied doorbell. There should only be one in the DB.
-                    DoorBell.findOne({ doorBellID: data.doorbellID }, function (err, doorbell) {
+                    DoorBell.findOne({ doorBellID: data.body.doorbellID }, function (err, doorbell) {
                         if(err) return console.error(err);
 
                         if(doorbell == null){
                             mongoose.disconnect();
-                            return console.log('Could not find doorbellID ' + data.doorbellID + ' notification from unregistered device');
+                            return console.log('Could not find doorbellID ' + data.body.doorbellID + ' notification. This is an unregistered device');
                         }
 
                         for(var user in doorbell.users){
                             for(var device in user.devices){
                                 push.wns.sendToastText04(device.channel, {
-                                text1: 'New Ring from DoorBell ' + data.doorBellID
+                                text1: 'New Ring from DoorBell ' + data.body.doorbellID
                                 }, {
                                         success: function(pushResponse) {
                                         console.log("Sent push:", pushResponse);
