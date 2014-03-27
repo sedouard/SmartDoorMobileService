b9ed5fc2f6cdf7321da842806a0e63c5ba5d2c79
sedouard <steven.edouard1@gmail.com>
2014-03-26 21:36:14 -0700

    initial draft of doorbell ring listener pushing to to push notification channels

--- service/scheduler/doorbellringlistener.js
+++ service/scheduler/doorbellringlistener.js
@@ -1,10 +1,9 @@
 var azure = require('azure');
 var mongoose = require('mongoose');
+nconf.file({ file: __dirname + '/../shared/config.jsn' });
 function doorBellRingListener() {
 
-    var sb = azure.createServiceBusService("Endpoint=sb://dpeproject.servicebus.windows.net/;SharedAccessKeyName=servicepolicy;SharedAccessKey=Xn1mYsNIRj47xd25AKeVa2Ant6eLC+Br0xrNfqQbhO4=");
-    //var hub = new azure.NotificationHubService(nconf.get("SmartDoor.ServiceBus.DoorBellNotificationHubName"),
-    //    nconf.get("SmartDoor.ServiceBus.DoorBellNotificationConnectionString"));
+    var sb = azure.createServiceBusService(nconf.get("SmartDoor.Notifications.DoorbellServiceBus"));
     
     listenForMessages();
     
@@ -12,18 +11,44 @@ function doorBellRingListener() {
         sb.receiveQueueMessage("arduino", { timeoutIntervalInS: 90 }, 
         function(err, data) {
 			if(data){
+                var connectionString = nconf.get('SmartDoor.MongodbConnectionString');
+                console.log('Connecting to mongodb with connection string: ' + connectionString);
                 
-				push.wns.sendToastText04(device.channelUri, {
-                        text1: item.text
-                    }, {
-                        success: function(pushResponse) {
-                            console.log("Sent push:", pushResponse);
+                mongoose.connect(connectionString);
+                var db = mongoose.connection;
+                
+                db.on('error', console.error.bind(console, 'connection error:'));
+                db.once('open', function () {
+                    console.log("Sucessfully Logged into mongo");
+                    console.log('Looking for doorBellID ' + doorbellID + ' in mongo');
+        
+                    //Query for the speicfied doorbell. There should only be one in the DB.
+                    DoorBell.findOne({ doorBellID: doorbellID }, function (err, doorbell) {
+                        if(err) return console.error(err);
+
+                        if(doorbell == null){
+                            return callback('Could not find doorbellID ' + doorbellID);
+                        }
+
+                        for(var user in doorbell.users){
+                            for(var device in user.devices){
+                                push.wns.sendToastText04(device.channel, {
+                                text1: 'New Ring from DoorBell ' + doorBellID
+                                }, {
+                                        success: function(pushResponse) {
+                                        console.log("Sent push:", pushResponse);
+                                    }
+                                }); 
+                            }
                         }
                     });
-                 
-				
-			}else
+    				
+                });
+                listenForMessages();
+			}
+            else
             {
+                //no new messages
                 listenForMessages();
             }
 		});
