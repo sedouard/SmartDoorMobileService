db2417e5b6e546570723cc5d65d88db1e01f777a
windowsazure <windowsazure>
2014-03-27 02:48:31 +0000

    Committing update from request /scmvfs/service/api/putobject.js

--- service/scheduler/doorbellringlistener.js
+++ service/scheduler/doorbellringlistener.js
@@ -2,6 +2,8 @@ var azure = require('azure');
 function doorBellRingListener() {
 
     var sb = azure.createServiceBusService("Endpoint=sb://dpeproject.servicebus.windows.net/;SharedAccessKeyName=servicepolicy;SharedAccessKey=Xn1mYsNIRj47xd25AKeVa2Ant6eLC+Br0xrNfqQbhO4=");
+    //var hub = new azure.NotificationHubService(nconf.get("SmartDoor.ServiceBus.DoorBellNotificationHubName"),
+    //    nconf.get("SmartDoor.ServiceBus.DoorBellNotificationConnectionString"));
     
     listenForMessages();
     
