f76098a5e6ceac98d26984f73e292da4af168601
sedouard <steven.edouard1@gmail.com>
2014-04-01 01:32:59 -0700

    Redo more work azure undid. :-/

--- service/scheduler/doorbellringlistener.js
+++ service/scheduler/doorbellringlistener.js
@@ -1,5 +1,7 @@
 var azure = require('azure');
 var nconf = require('nconf');
+var mongoose = require('mongoose');
+
 function doorBellRingListener() {
 
     var sb = azure.createServiceBusService("Endpoint=sb://dpeproject.servicebus.windows.net/;SharedAccessKeyName=servicepolicy;SharedAccessKey=Xn1mYsNIRj47xd25AKeVa2Ant6eLC+Br0xrNfqQbhO4=");
