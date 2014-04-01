8d729707f6c69a78d33595a97849c429db2ec4a6
windowsazure <windowsazure>
2014-04-01 06:35:47 +0000

    Committing update from request /scmvfs/service/api/photo.js

--- service/api/photo.js
+++ service/api/photo.js
@@ -27,7 +27,7 @@ exports.get = function(request, response) {
     var host = accountName + '.blob.core.windows.net';
     
     console.log('Connecting to blob service account: ' + accountName);
-    var blobService = azure=createBlobService(accountName, accountKey, host);
+    var blobService = azure.createBlobService(accountName, accountKey, host);
     blobService.createContainerIfNotExists(containerName
         , { publicAccessLevel: 'blob' }
         , function (error) {
