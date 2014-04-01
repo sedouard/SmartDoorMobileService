7c194496aaae4a337c528bbe6c44c7b035efc518
windowsazure <windowsazure>
2014-04-01 23:10:18 +0000

    Committing update from request /scmvfs/service/api/photo.js

--- service/api/photo.js
+++ service/api/photo.js
@@ -8,7 +8,7 @@ var nconf = require('nconf');
 //Get the doorbell model. This function will take care of making sure it hasn't already
 //been compiled
 var DoorBell = mongoosechemas.DoorBell;
-var Photo = mongoosechemas.Photo;
+
 
 
 //get config settings from azure mobile config dashboard page
@@ -101,10 +101,10 @@ function addPhotoToDoorbell(doorbellID, photoId, callback) {
                 console.log('debug: doorbell has no photo property, creating...');
                 doorbell.photos = new Array();
             }
-            doorbell.photos.push( new photo{
+            doorbell.photos.push( new DoorBell.{
                 blobPointer: photoId ,
                 timeStamp: date.getMilliseconds(),
-                url: 'http://' + host + '/' + containerName + '/' + photoId +'.jpg')
+                url: 'http://' + host + '/' + containerName + '/' + photoId +'.jpg'
             });
 
             doorbell.save(function (err) {
