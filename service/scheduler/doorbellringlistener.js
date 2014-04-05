775464474b1a937064716e9da29e39a7bd47daec
windowsazure <windowsazure>
2014-04-05 07:41:26 +0000

    Committing update from request /scmvfs/service/scheduler/doorbellringlistener.js

--- service/scheduler/doorbellringlistener.js
+++ service/scheduler/doorbellringlistener.js
@@ -1,3 +1,93 @@
-function doorbellringlistener() {
-    console.warn("You are running an empty scheduled job. Update the script for job 'doorbellringlistener' or disable the job.");
+/**
+This task really only needs to run once because after that we'll be listeneing to the service bus forever.
+**WARNING** Set this script to run at most once every 3 months, or just 'On Demand' otherwise gradually
+your compute bill will go through the roof!
+**/
+function doorbellringlistener(){
+    var azure = require('azure');
+    var nconf = require('nconf');
+    var mongoose = require('mongoose');
+    var mongoosechemas = require('../shared/mongooschemas.js');
+
+    //Get the doorbell model. This function will take care of making sure it hasn't already
+    //been compiled
+    var DoorBell = mongoosechemas.DoorBell;
+    nconf.argv().env();
+
+    console.log('Doorbell Listener Started');
+
+    var sb = azure.createServiceBusService(nconf.get("SmartDoor.Notifications.DoorbellServiceBus"));
+    //var hub = new azure.NotificationHubService(nconf.get("SmartDoor.ServiceBus.DoorBellNotificationHubName"),
+    //    nconf.get("SmartDoor.ServiceBus.DoorBellNotificationConnectionString"));
+
+    listenForMessages();
+
+    function listenForMessages() {
+        //Listen for 59 seconds, this job runs for 60 seconds so we avoid having multiple invokations
+        sb.receiveQueueMessage("arduino", { timeoutIntervalInS: 90 }, 
+            function(err, data) { 
+                if(!err){
+                    //storage container settings
+                    var containerName = nconf.get('SmartDoor.Storage.PhotoContainerName');
+                    var accountName = nconf.get('SmartDoor.Storage.AccountName');
+                    var doorBellObj = JSON.parse(data.body);
+                    var imageUrl = accountName + '.blob.core.windows.net';
+                    //image url in blob storage
+                    imageUrl = 'http://' + imageUrl + '/' + containerName + '/' + doorBellObj.imageId+'.jpg';
+
+                    console.log('Recieved notification: ' + doorBellObj.doorBellID);
+                    console.log('with image ' + imageUrl);
+                    console.log('Connecting to mongodb');
+                    
+                    var db = mongoose.connection;
+                    
+                    var processNotifications = function(){
+                        DoorBell.findOne({ doorBellID: doorBellObj.doorBellID }, function (err, doorbell) {
+                            if(err) {
+                                return console.error(err);
+                            }
+                            if(doorbell == null){
+                                mongoose.disconnect();
+                                return console.log('Could not find doorbellID ' + doorBellObj.doorBellID + ' notification. This is an unregistered device');
+                            }
+                            var date = new Date();
+
+                            //TODO: Assign Names to doorbells
+                            for(var user in doorbell.users){
+                                for(var device in doorbell.users[user].mobileDevices){
+                                    if(doorbell.users[user].mobileDevices[device].channel){
+                                        push.wns.sendToastImageAndText03(doorbell.users[user].mobileDevices[device].channel, {
+                                            text1: 'New Ring from your DoorBell ' + doorBellObj.doorBellID,
+                                            text2: 'At ' + date.getHours() + ':' + date.getMinutes() + ' today',
+                                            image1src: imageUrl,
+                                            image1alt: imageUrl
+                                        }, {
+                                                success: function(pushResponse) {
+                                                console.log("Sent push:", pushResponse);
+                                            }
+                                        });
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    if(db.readyState = 1){
+                        processNotifications();
+                    } else{
+                        console.error('Reconnecting to database');
+
+                        var db = mongoose.connection;
+                        db.on('open', function(){
+                            processNotifications();
+                        });
+                        
+                        db.on('error', function(){
+                            console.error('Error connecting to mongodb');
+                        })
+                    }
+                }
+                
+                listenForMessages();
+            });
+    }
 }
