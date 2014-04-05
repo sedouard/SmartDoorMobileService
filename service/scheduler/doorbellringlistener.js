553bfaea091ee77910e52d6869a5bb3977b1a974
sedouard <steven.edouard1@gmail.com>
2014-04-04 23:21:27 -0700

    retry to connect to db if it isn't connected already.

--- service/scheduler/doorbellringlistener.js
+++ service/scheduler/doorbellringlistener.js
@@ -17,55 +17,71 @@ function doorBellRingListener() {
     function listenForMessages() {
         //Listen for 59 seconds, this job runs for 60 seconds so we avoid having multiple invokations
         sb.receiveQueueMessage("arduino", { timeoutIntervalInS: 59 }, 
-        function(err, data) { 
-            if(!err){
-                //storage container settings
-                var containerName = nconf.get('SmartDoor.Storage.PhotoContainerName');
-                var accountName = nconf.get('SmartDoor.Storage.AccountName');
-                var doorBellObj = JSON.parse(data.body);
-                var imageUrl = accountName + '.blob.core.windows.net';
-                //image url in blob storage
-                imageUrl = 'http://' + imageUrl + '/' + containerName + '/' + doorBellObj.imageId+'.jpg';
+            function(err, data) { 
+                if(!err){
+                    //storage container settings
+                    var containerName = nconf.get('SmartDoor.Storage.PhotoContainerName');
+                    var accountName = nconf.get('SmartDoor.Storage.AccountName');
+                    var doorBellObj = JSON.parse(data.body);
+                    var imageUrl = accountName + '.blob.core.windows.net';
+                    //image url in blob storage
+                    imageUrl = 'http://' + imageUrl + '/' + containerName + '/' + doorBellObj.imageId+'.jpg';
 
-                console.log('Recieved notification: ' + doorBellObj.doorBellID);
-                console.log('with image ' + imageUrl);
-                console.log('Connecting to mongodb');
-                
-                var db = mongoose.connection;
-                
-                if(db.readyState = 1){
-                    DoorBell.findOne({ doorBellID: doorBellObj.doorBellID }, function (err, doorbell) {
-                        if(err) {
-                            return console.error(err);
-                        }
-                        if(doorbell == null){
-                            mongoose.disconnect();
-                            return console.log('Could not find doorbellID ' + doorBellObj.doorBellID + ' notification. This is an unregistered device');
-                        }
-                        var date = new Date();
+                    console.log('Recieved notification: ' + doorBellObj.doorBellID);
+                    console.log('with image ' + imageUrl);
+                    console.log('Connecting to mongodb');
+                    
+                    var db = mongoose.connection;
+                    
+                    var processNotifications = function(){
+                        DoorBell.findOne({ doorBellID: doorBellObj.doorBellID }, function (err, doorbell) {
+                            if(err) {
+                                return console.error(err);
+                            }
+                            if(doorbell == null){
+                                mongoose.disconnect();
+                                return console.log('Could not find doorbellID ' + doorBellObj.doorBellID + ' notification. This is an unregistered device');
+                            }
+                            var date = new Date();
 
-                        //TODO: Assign Names to doorbells
-                        for(var user in doorbell.users){
-                            for(var device in doorbell.users[user].mobileDevices){
-                                if(doorbell.users[user].mobileDevices[device].channel){
-                                    push.wns.sendToastImageAndText03(doorbell.users[user].mobileDevices[device].channel, {
-                                        text1: 'New Ring from your DoorBell ' + doorBellObj.doorBellID,
-                                        text2: 'At ' + date.getHours() + ':' + date.getMinutes() + ' today',
-                                        image1src: imageUrl,
-                                        image1alt: imageUrl
-                                    }, {
-                                            success: function(pushResponse) {
-                                            console.log("Sent push:", pushResponse);
-                                        }
-                                    });
+                            //TODO: Assign Names to doorbells
+                            for(var user in doorbell.users){
+                                for(var device in doorbell.users[user].mobileDevices){
+                                    if(doorbell.users[user].mobileDevices[device].channel){
+                                        push.wns.sendToastImageAndText03(doorbell.users[user].mobileDevices[device].channel, {
+                                            text1: 'New Ring from your DoorBell ' + doorBellObj.doorBellID,
+                                            text2: 'At ' + date.getHours() + ':' + date.getMinutes() + ' today',
+                                            image1src: imageUrl,
+                                            image1alt: imageUrl
+                                        }, {
+                                                success: function(pushResponse) {
+                                                console.log("Sent push:", pushResponse);
+                                            }
+                                        });
+                                    }
                                 }
                             }
-                        }
-                    });
-                } else{
-                    console.error('Could not connect to database');
+                        });
+                    }
+                    if(db.readyState = 1){
+                        processNotifications();
+                    } else{
+                        console.error('Reconnecting to database');
+
+                        var db = mongoose.connection;
+                        db.on('open', function(){
+                            processNotifications();
+                        });
+                        
+                        db.on('error', function(){
+                            console.error('Error connecting to mongodb');
+                        })
+                    }
                 }
-            }
-        });
+            });
     }
+}
+
+function processNotifications() {
+
 }
