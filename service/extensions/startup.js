b314855165c43d521d1cc1af3d45b397887e8015
sedouard <steven.edouard1@gmail.com>
2014-04-07 19:49:24 -0700

    typo in name

--- service/extensions/startup.js
+++ service/extensions/startup.js
@@ -1,4 +1,4 @@
-var doorbellListener = require('../shared/doorbellListener.js');
+var doorbellListener = require('../shared/doorbellringlistener.js');
 exports.startup = function(context, done) {
 		console.log('Initializing Serivce...');
 		doorbellListener.startRingListener();
