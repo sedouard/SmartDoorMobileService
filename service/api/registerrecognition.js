798281be2223141d4a31c29174cb3efb9f929331
sedouard <steven.edouard1@gmail.com>
2014-04-30 22:50:41 -0700

    adding lambda face recognition api integration

--- service/api/registerrecognition.js
+++ service/api/registerrecognition.js
@@ -1,4 +1,6 @@
 var nconf = require('nconf');
+var https = require('https');
+
 nconf.argv().env();
 
 exports.post = function(request, response) {
@@ -6,7 +8,33 @@ exports.post = function(request, response) {
     //   var tables = request.service.tables;
     //   var push = request.service.push;
 
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    //make request to lambda api
+    console.log('Executing registration request for userid ' + request.body.userid);
+
+	var options = {
+	  hostname: 'lambda-face-recognition.p.mashape.com',
+	  port: 443,
+	  path: '/album_train?album=' +nconf.get('SmartDoor.Identification.AlbumName') + '&albumkey=' + nconf.get('SmartDoor.Identification.AlbumKey') +
+	  '&entryid=' + request.body.userid + '&urls=' + request.body.photos,
+	  method: 'POST',
+	  headers: {'X-Mashape-Authorization':nconf.get('SmartDoor.Identification.ApiKey')}
+	};
+
+	var req = https.request(options, function(res) {
+	  console.log("statusCode: ", res.statusCode);
+	  console.log("headers: ", res.headers);
+
+	  res.on('data', function(d) {
+	    process.stdout.write(d);
+	  });
+	});
+	req.end();
+
+	req.on('error', function(e) {
+	  console.error(e);
+	});
+	
+	response.send(statusCodes.OK, { message : 'Hello World!' });
 };
 
 exports.get = function(request, response) {
