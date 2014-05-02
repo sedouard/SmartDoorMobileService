9a36d9f73f74b673f39b74d6ad77b8b3481df411
windowsazure <windowsazure>
2014-05-02 03:28:06 +0000

    Committing update from request /scmvfs/service/api/trackedusers.js

--- service/api/trackedusers.js
+++ service/api/trackedusers.js
@@ -7,7 +7,7 @@ exports.get = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
     //   var tables = request.service.tables;
     //   var push = request.service.push;
-    var doorBellID = request.query.doorbellId;
+    var doorBellID = request.query.doorbellID;
     
     var db = mongoose.connection;
     
