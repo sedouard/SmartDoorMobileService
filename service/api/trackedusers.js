59ae0672e7aeea07590ac997b6f27453075b1b90
windowsazure <windowsazure>
2014-05-02 03:14:15 +0000

    Committing update from request /scmvfs/service/api/trackedusers.js

--- service/api/trackedusers.js
+++ service/api/trackedusers.js
@@ -2,7 +2,7 @@ exports.post = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
     //   var tables = request.service.tables;
     //   var push = request.service.push;
-
+    
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
 
