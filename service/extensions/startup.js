46d7719890d98f93108212e2b380b4615f92b9ec
sedouard <steven.edouard1@gmail.com>
2014-05-19 13:46:38 -0700

    remove the doorbell listener at startup and place it back in the scheduler

--- service/extensions/startup.js
+++ service/extensions/startup.js
@@ -1,6 +1,6 @@
 var doorbellListener = require('../shared/doorbellringlistener.js');
 exports.startup = function(context, done) {
 		console.log('Initializing Serivce...');
-		doorbellListener.startRingListener();
+		//doorbellListener.startRingListener();
 		done();
 	};
