a4aa0bf1105afb571ac495de6d6a71aa5fdec0ff
windowsazure <windowsazure>
2014-05-23 00:18:08 +0000

    Committing update from request /scmvfs/service/scheduler/doorbellListener.js

--- service/scheduler/doorbellListener.js
+++ service/scheduler/doorbellListener.js
@@ -115,6 +115,7 @@ function doorbellListener() {
                                 console.error(error);
                             });
                        //update tiles
+                       console.log('Sending Tile update');
                        hub.wns.sendTileSquare150x150PeekImageAndText03(doorBellObj.doorBellID, {
                                 text1: msg + ' just rang!',
                                 image11: imageUrl
@@ -122,7 +123,10 @@ function doorbellListener() {
                             function(error){
                                 if(!error){
                                     console.log("Sent tile update!");
+                                    return;
                                 }
+                                
+                                console.log(error);
                             }
                        );
                     }
