2b91c4d3d60fa4ffb805642697bc7d450d457687
windowsazure <windowsazure>
2014-05-23 05:20:34 +0000

    Committing update from request /scmvfs/service/api/longtermtoken.js

--- service/api/longtermtoken.js
+++ service/api/longtermtoken.js
@@ -6,7 +6,15 @@ exports.get = function(request, response) {
         return response.send(400, {message: "must provide access_token uri parameter" } );
     }
     
-    var req = unirest.get("https://graph.facebook.com/oauth/access_token?=" + request.)
-    response.send(statusCodes.OK, { message : 'Hello World!' });
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    var req = unirest.get("https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=" + nconf.get("SmartDoor.Facebook.ClientId") + "&client_secret=" + nconf.get("SmartDoor.Facebook.ClientSecret") + "&fb_exchange_token=" + request.query.access_token);
+    req
+    .send()
+    .end(function(response) {
+        if(response.body.access_token){
+            response.send(statusCodes.OK, {access_token: response.body.access_token } );
+        }
+        else{
+            response.send(500, { message: "Could not exchange access tokens" });
+        }
+    });
 };
